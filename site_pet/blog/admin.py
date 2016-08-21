c348688e3520d1e0ec2beea48ca510805c2d221f
luisccf <luisccf@me.com>
2016-08-21 10:24:54 -0300

    Adds proxy model for publications

--- site_pet/blog/admin.py
+++ site_pet/blog/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
-from blog.models import Publication
+from blog.models import Publication, MyPublication
 from django_summernote.admin import SummernoteModelAdmin
+from django.core.exceptions import ValidationError
+from django.contrib import messages
+from django import forms
+
+# class PublicationForm(forms.ModelForm):
+#     class Meta:
+#         model = Publication
+#         fields = '__all__'
+
+#     def clean(self):
+#         import pdb; pdb.set_trace()
+#         return self.cleaned_data
 
 class PublicationAdmin(SummernoteModelAdmin):
-	pass
+    def get_queryset(self, request):
+        return Publication.objects.all()
+    def save_model(self, request, obj, form, change):
+        if change is False:
+            obj.user = request.user.get_username()
+        else:
+            if obj.user != request.user.get_username():
+                raise ValidationError('You can\t edit this')
+        obj.save()
 
+class MyPublicationAdmin(PublicationAdmin):
+    def get_queryset(self, request):
+        return MyPublication.objects.filter(user=request.user.get_username())
+
+
+admin.site.register(MyPublication, MyPublicationAdmin)
 admin.site.register(Publication, PublicationAdmin)
--- site_pet/blog/models.py
+++ site_pet/blog/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 import os, datetime
+from django.views.generic import TemplateView
 
 def get_image_path(instance, filename):
     return os.path.join('blog/thumbnails', datetime.datetime.now().strftime('%Y%m%d%H%M%S'), filename)
 
 class Publication(models.Model):
-	title = models.CharField(max_length=255)
-	author = models.CharField(max_length=255)
-	text_call = models.CharField(max_length=255)
-	text_content = models.TextField()
-	thumbnail = models.ImageField(upload_to=get_image_path)
-	publish_date = models.DateField(auto_now=False, auto_now_add=True)
-	last_modification = models.DateField(auto_now=True, auto_now_add=False)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    user = models.CharField(max_length=255, editable=False)
+    text_call = models.CharField(max_length=255)
+    text_content = models.TextField()
+    thumbnail = models.ImageField(upload_to=get_image_path)
+    publish_date = models.DateField(auto_now=False, auto_now_add=True)
+    last_modification = models.DateField(auto_now=True, auto_now_add=False)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
+class MyPublication(Publication):
+    class Meta:
+        proxy = True

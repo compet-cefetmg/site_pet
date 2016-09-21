9764b51a872235e8b6be607a500da74cf6c1a8e6
luisccf <luisccf@me.com>
2016-09-21 20:36:31 -0300

    Creates campus model

--- site_pet/pets/admin.py
+++ site_pet/pets/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-
-from pets.models import Pet
+from pets.models import Pet, Campus
+from utils.decimal_to_roman import write_roman 
 
 class PetInline(admin.StackedInline):
     model = Pet
     can_delete = False
-    verbose_name_plural = 'Informações adicionais'
+    verbose_name_plural = 'Pet Info'
+
 
 class UserAdmin(BaseUserAdmin):
     inlines = (PetInline, )
 
+
+class CampusAdmin(admin.ModelAdmin):
+	def save_model(self, request, obj, form, change):
+		import pdb; pdb.set_trace()
+		obj.roman_id = write_roman(obj.id)
+		obj.save()
+
+
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+admin.site.register(Campus, CampusAdmin)
--- site_pet/pets/models.py
+++ site_pet/pets/models.py
@@ -6,12 +6,24 @@ import datetime
 
 
 class Pet(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	photo = models.ImageField(upload_to=get_image_path)
-	campus = models.IntegerField()
-	course = models.CharField(max_length=255)
-	start = models.DateField()
-	description = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to=get_image_path)
+    campus = models.IntegerField()
+    course = models.CharField(max_length=255)
+    start = models.DateField()
+    description = models.TextField()
 
-	def __str__(self):
-		return self.course + ' (Campus ' + str(self.campus) + ')'
+    def __str__(self):
+        return self.course + ' (Campus ' + str(self.campus) + ')'
+
+
+class Campus(models.Model):
+    class Meta:
+        verbose_name_plural = 'campi'
+
+    id = models.IntegerField(primary_key=True, verbose_name='Number')
+    location = models.CharField(max_length=255)
+    roman_id = models.CharField(max_length=2, editable=False)
+
+    def __str__(self):
+        return 'Campus ' + self.roman_id + ' (' + self.location + ')'

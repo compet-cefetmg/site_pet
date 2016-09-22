b38ab60be21da883e94737b0fd20329d62f97ef1
luisccf <luisccf@me.com>
2016-09-21 23:04:16 -0300

    Maps members url

--- site_pet/members/views.py
+++ site_pet/members/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    context = {'members': Member.objects.all()}
+    return render(request, 'members/index.html', context)
--- site_pet/site_pet/urls.py
+++ site_pet/site_pet/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^summernote/', include('django_summernote.urls')),
     url(r'^blog/', include('blog.urls')),
+    url(r'^members/', include('members.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

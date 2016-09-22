bfd89f8fc7f6da1523288ba5f368142d51e4491d
luisccf <luisccf@me.com>
2016-09-22 00:19:39 -0300

    Renders members according to url

--- site_pet/members/urls.py
+++ site_pet/members/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index),
+    url(r'^(?P<username>[a-zA-Z0-9]+)$', views.index),
 ]
--- site_pet/members/views.py
+++ site_pet/members/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.contrib.auth.models import User
+from django.shortcuts import get_list_or_404
 
 
-def index(request):
-    context = {'members': Member.objects.all()}
+def index(request, username=""):
+    user = get_list_or_404(User, username=username)
+    members = Member.objects.filter(user=user[0])
+    context = {'members': members }
     return render(request, 'members/index.html', context)

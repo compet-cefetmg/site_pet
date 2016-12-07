183b9d07d2c4dbfde6857192893035fab2501f78
luisccf <luisccf@me.com>
2016-12-07 21:34:41 -0200

    Adds login error message

--- site_pet/staff/forms.py
+++ site_pet/staff/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from django.contrib.auth import authenticate
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Usuário', max_length=100)
-    password = forms.CharField(label='Senha', max_length=100, widget=forms.PasswordInput())
+    username = forms.CharField(label='Usuário', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Senha', max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     def login(self, request):
         username = self.data.get('username')
--- site_pet/staff/views.py
+++ site_pet/staff/views.py
@@ -6,21 +6,22 @@ from django.contrib.auth import authenticate, login, logout
 
 def auth_login(request):
     form = LoginForm(request.POST or None)
+    context = {'form': form, 'name': 'staff.login'}
     if request.POST and form.is_valid:
         user = form.login(request)
         if user:
             login(request, user)
-            if request.GET['next']:
+            if 'next' in request.GET:
                 return redirect(request.GET['next'])
-            else:
-                return render(request)
-
-    return render(request, 'staff/login.html', {'form': form, 'name': 'staff.login'})
+            return redirect('/')
+        context['error'] = 'Usuário ou senha incorretos.'
+        return render(request, 'staff/login.html', context)
+    return render(request, 'staff/login.html', context)
 
 
 def auth_logout(request):
     logout(request)
-    return HttpResponse('OK!')
+    return redirect('/')
 
 def index(request):
     return render(request, 'staff/index.html', {'name': 'staff.index'})

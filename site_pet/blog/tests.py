ca677b6004095818591ef88925aee6291958bec1
luisccf <luisccf@me.com>
2016-12-09 08:31:40 -0200

    Adds add post test case

--- site_pet/blog/tests.py
+++ site_pet/blog/tests.py
@@ -4,39 +4,22 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
 from faker import Faker
-from django.utils.six import BytesIO  
-from PIL import Image  
+from django.utils.six import BytesIO
+from PIL import Image
 from members.models import Member
-
 
 faker = Faker()
 
 
-def create_image(storage, filename, size=(100, 100), image_mode='RGB', image_format='PNG'):
-    """
-    Generate a test image, returning the filename that it was saved as.
-
-    If ``storage`` is ``None``, the BytesIO containing the image data
-    will be passed instead.
-    """
-    data = BytesIO()
-    Image.new(image_mode, size).save(data, image_format)
-    data.seek(0)
-    if not storage:
-        return data
-    image_file = ContentFile(data.read())
-    return storage.save(filename, image_file)
-
-
 class PostTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user('admin', 'admin@admin.com', 'password')
 
+    def setUp(self):
+        self.user = User.objects.create_user(
+            'admin', 'admin@admin.com', 'password')
 
     def test_post_should_return_unauthorized(self):
         response = self.client.get(reverse('blog.add_post'))
         self.assertEqual(response.status_code, 302)
-
 
     def test_post_form_should_be_loaded(self):
         self.client.login(username='admin', password='password')
@@ -44,25 +27,23 @@ class PostTestCase(TestCase):
         response = self.client.get(reverse('blog.add_post'))
         self.assertEqual(response.status_code, 200)
 
-
     def test_post_should_not_be_created(self):
         self.client.login(username='admin', password='password')
         self.assertTrue(self.user.is_authenticated())
         response = self.client.post(reverse('blog.add_post'))
-        self.assertEqual(response.status_code, 400)    
+        self.assertEqual(response.status_code, 400)
 
-
-    # def test_post_should_be_created(self):
-    #     self.client.login(username='admin', password='password')
-    #     self.assertTrue(self.user.is_authenticated())
-    #     avatar = create_image(None, 'mediafiles/thumb.png')
-    #     thumbnail = SimpleUploadedFile('thumb.png', avatar.getvalue())
-    #     post = {
-    #         'text_call': faker.sentence(),
-    #         'title': faker.sentence(),
-    #         'author': self.user.id,
-    #         'text_content': faker.text(),
-    #         'thumbnail': thumbnail
-    #     }
-    #     response = self.client.post(reverse('blog.add_post'), post)
-    #     self.assertEqual(response.status_code, 200)
+    def test_post_should_be_created(self):
+        self.client.login(username='admin', password='password')
+        self.assertTrue(self.user.is_authenticated())
+        thumbnail = SimpleUploadedFile(name='thumb.png', content=open(
+            'mediafiles/thumb.png', 'rb').read(), content_type='image/png')
+        post = {
+            'text_call': faker.sentence(),
+            'title': faker.sentence(),
+            'author': faker.name(),
+            'text_content': faker.text(),
+            'thumbnail': thumbnail
+        }
+        response = self.client.post(reverse('blog.add_post'), post)
+        self.assertEqual(response.status_code, 302)

32e24795cd9e4a44e80142dde768e6c8d094c064
luisccf <luisccf@me.com>
2017-01-08 12:28:30 -0200

    Adds edit role button

--- site_pet/static/js/staff/tutor_index.js
+++ site_pet/static/js/staff/tutor_index.js
@@ -1,4 +1,42 @@
+function initMembersDataTable(tableId, urlPrefix) {
+    var table = $('#'+tableId).DataTable({
+        'sAjaxSource': urlPrefix + 'all',
+        'dom': 'ft',
+        'language': {
+            'url': 'https://cdn.datatables.net/plug-ins/1.10.13/i18n/Portuguese-Brasil.json'
+        },
+        // Renders title row with anchor tag
+        'columnDefs': [
+        // Renders edit and remove buttons
+            {
+                "targets": 4,
+                "data": null,
+                'render': function (data, type, row, meta) {
+                    var editRoleBtn = $($.parseHTML('<a></a>'));
+                    editRoleBtn
+                        .attr('href', '/members/' + data[2] + '/edit')
+                        .attr('title', 'Alterar papel')
+                        .addClass('edit-btn')
+                        .append($.parseHTML('<i class="fa fa-pencil"></i>'));
+                    return data[4] + editRoleBtn[0].outerHTML
+                }
+            },
+            {
+                "targets": 0,
+                "visible": false,
+                "searchable": false
+            }
+        ]
+    });
+
+    $('#'+tableId +' tbody').on('click', '.edit-btn', function() {
+        var data = table.row($(this).parents('tr')).data();
+        window.location.href = urlPrefix + data[0] + '/edit';
+    });
+}
+
+
 $(function() {
     initDataTable('posts', '/blog/post/');
-    initDataTable('members', '/members/member/');
+    initMembersDataTable('members', '/members/member/');
 });

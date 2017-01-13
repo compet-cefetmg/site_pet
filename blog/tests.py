0c06b17123c55b42e384206d94f1ff756fb83289
luisccf <luisccf@me.com>
2017-01-13 11:38:00 -0200

    Fixes pep8 offenses

--- blog/tests.py
+++ blog/tests.py
@@ -41,8 +41,7 @@ class PostTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_post_form_should_be_loaded(self):
-        self.client.login(username=self.member.user.username,
-                          password='password')
+        self.client.login(username=self.member.user.username, password='password')
         self.assertTrue(self.member.user.is_authenticated())
         response = self.client.get(reverse('blog.add_post'))
         self.assertEqual(response.status_code, 200)
@@ -55,8 +54,7 @@ class PostTestCase(TestCase):
         self.assertEqual(response.status_code, 400)
 
     def test_post_should_be_created(self):
-        self.client.login(username=self.member.user.username,
-                          password='password')
+        self.client.login(username=self.member.user.username, password='password')
         self.assertTrue(self.member.user.is_authenticated())
         thumbnail = SimpleUploadedFile(name='thumb.png', content=open(
             'mediafiles/thumb.png', 'rb').read(), content_type='image/png')
@@ -71,29 +69,23 @@ class PostTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_post_should_be_deleted_by_author(self):
-        self.client.login(username=self.member.user.username,
-                          password='password')
+        self.client.login(username=self.member.user.username, password='password')
         post = PostFactory(member=self.member)
-        response = self.client.post(
-            reverse('blog.delete_post'), {'id': post.id})
+        response = self.client.post(reverse('blog.delete_post'), {'id': post.id})
         self.assertEqual(response.status_code, 200)
 
     def test_post_should_be_deleted_by_pet_member(self):
         pet_member = MemberFactory(pet=self.member.pet)
-        self.client.login(username=pet_member.user.username,
-                          password='password')
+        self.client.login(username=pet_member.user.username, password='password')
         post = PostFactory(member=self.member)
-        response = self.client.post(
-            reverse('blog.delete_post'), {'id': post.id})
+        response = self.client.post(reverse('blog.delete_post'), {'id': post.id})
         self.assertEqual(response.status_code, 200)
 
     def test_post_should_not_be_deleted_by_non_pet_member(self):
         post = PostFactory(member=self.member)
         other_member = MemberFactory()
-        self.client.login(username=other_member.user.username,
-                          password='password')
-        response = self.client.post(
-            reverse('blog.delete_post'), {'id': post.id})
+        self.client.login(username=other_member.user.username, password='password')
+        response = self.client.post(reverse('blog.delete_post'), {'id': post.id})
         self.assertEqual(response.status_code, 400)
 
     def test_json_should_have_data_attr_with_array_of_4_elements(self):
--- staff/tests.py
+++ staff/tests.py
@@ -4,6 +4,7 @@ from factories.member_factory import MemberFactory
 
 
 class LoginTestCase(TestCase):
+
     def setUp(self):
         AdminGroupFactory()
         self.tutor = MemberFactory()
@@ -11,16 +12,13 @@ class LoginTestCase(TestCase):
         self.tutor.user.groups.add(TutorsGroupFactory())
         self.member.user.groups.add(MembersGroupFactory())
 
-
     def test_should_login_with_success_and_redirect(self):
         response = self.client.post('/staff/login', {'username': self.member.user.username, 'password': 'password'})
         self.assertEqual(response.status_code, 302)
 
-
     def test_should_not_login_and_reload_page(self):
         response = self.client.post('/staff/login', {'username': self.member.user.username, 'password': 'pwd'})
         self.assertEqual(response.status_code, 401)
-
 
     def test_should_logout_with_success(self):
         response = self.client.get('/staff/logout')

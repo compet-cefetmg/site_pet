4857fe952d94805c7da50d6adb3e5e30934851b5
luisccf <luisccf@me.com>
2017-01-14 07:56:50 -0200

    Fixes after login redirect

--- staff/views.py
+++ staff/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from staff.forms import LoginForm
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
@@ -7,15 +7,15 @@ from django.contrib.auth.models import Group
 
 def auth_login(request):
     form = LoginForm(request.POST or None)
-    context = {'form': form, 'name': 'staff.login'}
+    context = {'form': form, 'name': 'staff.auth_login'}
     if request.POST and form.is_valid:
         user = form.login(request)
         if user:
             groups = [Group.objects.get(name='admin'), Group.objects.get(name='tutors'), Group.objects.get(name='members')]
             if not groups[0] in user.groups.all() and not groups[1] in user.groups.all() and not groups[2] in user.groups.all():
-                return HttpResponse('Unauthorized', status=401)
+                return HttpResponse('Este usuário não é um administrador, tutor ou membro de algum PET e, portanto, não pode acessar o sistema.', status=403)
             login(request, user)
-            if 'next' in request.GET:
+            if 'next' in request.GET and request.GET.get('next', ''):
                 return redirect(request.GET['next'], permanent=True)
             return redirect('/')
         context['error'] = 'Usuário ou senha incorretos.'
@@ -31,13 +31,11 @@ def auth_logout(request):
 def index(request):
     user_groups = request.user.groups.all()
     if Group.objects.get(name='admin') in user_groups:
-        return render(request, 'staff/admin_index.html', {'name': 'staff.index'})
+        return render(request, 'staff/admin_index.html')
     if Group.objects.get(name='tutors') in user_groups:
-        return render(request, 'staff/tutors_index.html', {'name': 'staff.index'})
+        return render(request, 'staff/tutors_index.html')
     if Group.objects.get(name='members') in user_groups:   
-        return render(request, 'staff/member_index.html', {'name': 'staff.index'})
-    return HttpResponse('Unauthorized', status=401)
-
-
-
-
+        return render(request, 'staff/member_index.html')
+    if request.user.is_authenticated():
+        return HttpResponse('Este usuário não é um administrador, tutor ou membro de algum PET e, portanto, não pode acessar o sistema.', status=403)
+    return redirect(reverse('staff.auth_login'))

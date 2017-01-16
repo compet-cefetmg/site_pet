d1a7e44ad01fa8a46a6a92540db2cd17e24c735a
luisccf <luisccf@me.com>
2017-01-16 12:13:13 -0200

    Adds blog tests

--- blog/tests.py
+++ blog/tests.py
@@ -7,9 +7,16 @@ from faker import Faker
 from factories.member_factory import *
 from factories.auth_factory import *
 from factories.blog_factory import PostFactory
+from blog.models import Post
 
 
 faker = Faker()
+
+
+def generate_post_form_data():
+    thumbnail = SimpleUploadedFile(name='thumb.png', content=open(
+        'mediafiles/thumb.png', 'rb').read(), content_type='image/png')
+    return {'text_call': faker.sentence(), 'title': faker.sentence(), 'text_content': faker.text(), 'thumbnail': thumbnail}
 
 
 class PostTestCase(TestCase):
@@ -18,69 +25,111 @@ class PostTestCase(TestCase):
         self.roles = {}
         for role in ['admin', 'tutor', 'scholar', 'volunteer', 'contributor', 'ex-member']:
             self.roles[role] = MemberRoleFactory(name=role)
+
         self.member = MemberFactory(role=self.roles['scholar'])
-        self.posts = PostFactory.create_batch(5)
 
     def test_index_page_should_return_200(self):
         response = self.client.get(reverse('blog.index'))
         self.assertEqual(response.status_code, 200)
 
-    def test_5_posts_should_be_loaded(self):
-        self.assertEqual(len(self.posts), 5)
-        for post in self.posts:
-            response = self.client.get(reverse('blog.post', kwargs={'id': post.id}))
-            self.assertEqual(response.status_code, 200)
+    def test_index_page_should_return_404(self):
+        response = self.client.get(reverse('blog.index'), {'pet': 0})
+        self.assertEqual(response.status_code, 404)
 
-    def test_should_return_no_post_found(self):
+    def test_index_page_should_return_given_pet_posts_only(self):
+        posts = PostFactory.create_batch(5, member=self.member)
+        PostFactory.create_batch(5)
+
+        response = self.client.get(reverse('blog.index'), {'pet': self.member.pet.id})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(5, len(response.context['posts']))
+
+        for post in response.context['posts']:
+            self.assertIn(post, posts)
+
+    def test_post_page_should_be_loaded(self):
+        post = PostFactory()
+
+        response = self.client.get(reverse('blog.post', kwargs={'id': post.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['post'], post)
+
+    def test_post_page_should_return_404(self):
         response = self.client.get(reverse('blog.post', kwargs={'id': 0}))
         self.assertEqual(response.status_code, 404)
 
-    def test_post_should_return_unauthorized(self):
+    def test_add_form_should_not_be_loaded(self):
         response = self.client.get(reverse('blog.add_post'))
         self.assertEqual(response.status_code, 302)
 
-    def test_post_form_should_be_loaded(self):
+    def test_add_form_should_be_loaded(self):
         self.client.login(username=self.member.user.username, password='password')
-        self.assertTrue(self.member.user.is_authenticated())
         response = self.client.get(reverse('blog.add_post'))
         self.assertEqual(response.status_code, 200)
 
-    def test_post_should_not_be_created(self):
-        self.client.login(username=self.member.user.username,
-                          password='password')
-        self.assertTrue(self.member.user.is_authenticated())
-        response = self.client.post(reverse('blog.add_post'))
-        self.assertEqual(response.status_code, 400)
+    def test_add_form_should_return_400(self):
+        self.client.login(username=self.member.user.username, password='password')
+        data = generate_post_form_data()
+
+        for key in data:
+            value = data.pop(key)
+            response = self.client.post(reverse('blog.add_post'), data)
+            self.assertEqual(response.status_code, 400)
+            data[key] = value
 
     def test_post_should_be_created(self):
         self.client.login(username=self.member.user.username, password='password')
-        self.assertTrue(self.member.user.is_authenticated())
-        thumbnail = SimpleUploadedFile(name='thumb.png', content=open(
-            'mediafiles/thumb.png', 'rb').read(), content_type='image/png')
-        post = {
-            'text_call': faker.sentence(),
-            'title': faker.sentence(),
-            'author': faker.name(),
-            'text_content': faker.text(),
-            'thumbnail': thumbnail,
-        }
-        response = self.client.post(reverse('blog.add_post'), post)
+        response = self.client.post(reverse('blog.add_post'), generate_post_form_data())
         self.assertEqual(response.status_code, 302)
 
-    def test_post_should_be_deleted_by_author(self):
-        self.client.login(username=self.member.user.username, password='password')
+    def test_edit_form_should_return_not_be_loaded(self):
         post = PostFactory(member=self.member)
+        self.client.login(username=MemberFactory().user.username, password='password')
+        response = self.client.get(reverse('blog.edit_post', kwargs={'id': post.id}))
+        self.assertEqual(response.status_code, 403)
+
+    def test_edit_form_should_be_loaded(self):
+        post = PostFactory(member=self.member)
+        self.client.login(username=self.member.user.username, password='password')
+        response = self.client.get(reverse('blog.edit_post', kwargs={'id': post.id}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_form_should_be_invalid(self):
+        post = PostFactory(member=self.member)
+        self.client.login(username=self.member.user.username, password='password')
+        response = self.client.post(reverse('blog.edit_post', kwargs={'id': post.id}))
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_should_be_edited(self):
+        post = PostFactory(member=self.member)
+        data = generate_post_form_data()
+
+        self.client.login(username=self.member.user.username, password='password')
+        response = self.client.post(reverse('blog.edit_post', kwargs={'id': post.id}), data)
+        self.assertEqual(response.status_code, 302)
+
+        edited_post = Post.objects.get(id=post.id)
+        self.assertEqual(edited_post.text_call, data['text_call'])
+        self.assertEqual(edited_post.title, data['title'])
+        self.assertEqual(edited_post.text_content, data['text_content'])
+        self.assertEqual(edited_post.member, self.member)
+
+    def test_post_should_be_deleted_by_author(self):
+        post = PostFactory(member=self.member)
+
+        self.client.login(username=self.member.user.username, password='password')
         response = self.client.post(reverse('blog.delete_post'), {'id': post.id})
         self.assertEqual(response.status_code, 200)
 
     def test_post_should_be_deleted_by_pet_member(self):
         pet_member = MemberFactory(pet=self.member.pet)
-        self.client.login(username=pet_member.user.username, password='password')
         post = PostFactory(member=self.member)
+
+        self.client.login(username=pet_member.user.username, password='password')
         response = self.client.post(reverse('blog.delete_post'), {'id': post.id})
         self.assertEqual(response.status_code, 200)
 
-    def test_post_should_not_be_deleted_by_non_pet_member(self):
+    def test_post_should_not_be_deleted_by_other_pet_member(self):
         post = PostFactory(member=self.member)
         other_member = MemberFactory()
         self.client.login(username=other_member.user.username, password='password')
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseForbidden
 from .models import Post
 from .forms import PostForm
 from cefet.models import Pet
@@ -14,8 +14,7 @@ def index(request):
     pet_id = request.GET.get('pet', '')
     if pet_id:
         pet = get_object_or_404(Pet, id=pet_id)
-        posts = Post.objects.filter(
-            member__in=pet.members.all()).order_by('publish_date').all()
+        posts = Post.objects.filter(member__in=pet.members.all()).order_by('publish_date').all()
     else:
         posts = Post.objects.order_by('publish_date').all()
     context = {'posts': posts, 'name': 'blog.index'}
@@ -31,46 +30,53 @@ def post(request, id):
 def all(request):
     posts = [(p.id, p.title, p.member.name, p.publish_date.strftime("%d/%m/%y"))
              for p in Post.objects.filter(member__in=request.user.member.pet.members.all()).all()]
-    json = {'data': posts}
-    return JsonResponse(json, safe=False)
+    return JsonResponse({'data': posts}, safe=False)
 
 
 @login_required
 def add_post(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
+
         if form.is_valid():
             post = form.save()
             post.member = request.user.member
             post.save()
+
+            messages.success(request, 'Post editado com sucesso.')
             return redirect(reverse('staff.index'))
-        context = {'name': 'blog.add_post', 'form': form}
-        return render(request, 'blog/form.html', context, status=400)
-    else:
-        form = PostForm()
-        context = {'name': 'blog.add_post', 'form': form}
-        return render(request, 'blog/form.html', context)
+
+        return render(request, 'blog/form.html', {'form': form}, status=400)
+
+    return render(request, 'blog/form.html', {'form': PostForm()})
 
 
 @login_required
 def edit_post(request, id):
     post = get_object_or_404(Post, id=id)
+
     if post.member.pet != request.user.member.pet:
-        return HttpResponse('Unauthorized', status=400)
+        return HttpResponseForbidden('Você só pode editar posts de seu próprio PET.')
+
     if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
+        form = PostForm(request.POST, request.FILES, instance=post)
+
         if form.is_valid():
-            post = form.save()
+            form.save()
             messages.success(request, 'Post editado com sucesso.')
             return redirect(reverse('staff.index'))
+
         return render(request, 'blog/form.html', {'form': form}, status=400)
+
     return render(request, 'blog/form.html', {'form': PostForm(instance=post)})
 
 
 @login_required
 def delete_post(request):
     post = get_object_or_404(Post, id=request.POST.get('id'))
+
     if post.member.pet != request.user.member.pet or request.method != 'POST':
-        return HttpResponse('Você só pode remover posts de seu próprio PET.', status=403)
+        return HttpResponseForbidden('Você só pode remover posts de seu próprio PET.')
+
     post.delete()
-    return HttpResponse('OK', status=200)
+    return HttpResponse()
